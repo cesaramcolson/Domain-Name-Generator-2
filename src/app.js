5c557a9eaa9faef0bb96eafc43605870abff8721
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-12-07 15:12:15 +0000

    domain name generator done, still to work on the hacks

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjs = ["great", "big", "last"];
+  let nouns = ["jogger", "racoon", "Ofus"];
+  let extentions = [".com", ".us", ".net", ".io"];
+
+  for (let pronoun in pronouns) {
+    for (let adj in adjs) {
+      for (let noun in nouns) {
+        for (let extention in extentions) {
+          console.log(
+            pronouns[pronoun] + adjs[adj] + nouns[noun] + extentions[extention]
+          );
+        }
+      }
+    }
+  }
 };
